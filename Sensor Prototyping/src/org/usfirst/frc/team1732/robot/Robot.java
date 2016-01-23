493a3dd9a3ec3f1d06236d63c76d50fa1f1a47ea
JayShower <lacrossejason@gmail.com>
2016-01-23 14:05:29 -0600

    Update Robot.java

--- Sensor Prototyping/src/org/usfirst/frc/team1732/robot/Robot.java
+++ Sensor Prototyping/src/org/usfirst/frc/team1732/robot/Robot.java
@@ -1,8 +1,10 @@
 package org.usfirst.frc.team1732.robot;
 
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
+
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.vision.USBCamera;
 
 /**
  * This is a demo program showing the use of the CameraServer class.
@@ -12,9 +14,11 @@ import edu.wpi.first.wpilibj.vision.USBCamera;
 public class Robot extends SampleRobot {
 
 	DigitalInput[] DIO;
-	CameraServer server;
+	//CameraServer server;
 	Encoder encoder;
 	Joystick controller;
+	Image bob;
+	int billy;
 
     public Robot() {
 		controller = new Joystick(0);
@@ -23,22 +27,37 @@ public class Robot extends SampleRobot {
     		DIO[i] = new DigitalInput(i);
     	}
     	encoder = new Encoder(8, 9);
-    	server = CameraServer.getInstance();
-        server.setQuality(50);
-        server.startAutomaticCapture("cam0");
+    	//server = CameraServer.getInstance();
+        //server.setQuality(50);
+        //server.startAutomaticCapture("cam0");
+        bob = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+        billy = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        NIVision.IMAQdxConfigureGrab(billy);
+        SmartDashboard.putBoolean("Bla", false);
     }
     
     public void dioToDashboard(int port) {
     	SmartDashboard.putBoolean("DIO " + port, DIO[port].get());
     }
+    
+    public void sendImage(boolean theKid) {
+    	if(theKid) {
+    		NIVision.IMAQdxGrab(billy, bob, 1);
+    		CameraServer.getInstance().setImage(bob);
+    	}
+    }
 
     public void operatorControl() {
+    	NIVision.IMAQdxStartAcquisition(billy);
     	encoder.reset();
         while (isOperatorControl() && isEnabled()) {
+        	sendImage(SmartDashboard.getBoolean("Bla", false));
+        	//sendImage(controller.getRawButton(0));
         	for(int i = 0; i < 7; i++) dioToDashboard(i);
         	SmartDashboard.putNumber("Raw", encoder.getRaw());
             Timer.delay(0.005);		// wait for a motor update time
         }
+        NIVision.IMAQdxStopAcquisition(billy);
     }
     
 }
